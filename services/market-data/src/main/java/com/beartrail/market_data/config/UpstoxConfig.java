44321b50efece07420ab8e1942fa26f2951f9743
georgereuben <reubengeorge101@gmail.com>
2025-07-31 13:35:22 +0530

    add market data entity

---
+++ services/market-data/src/main/java/com/beartrail/market_data/config/UpstoxConfig.java
@@ -0,0 +1,13 @@
+package com.beartrail.market_data.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "upstox")
+public class UpstoxConfig {
+    private String authToken;
+    private String apiUrl;
+}
