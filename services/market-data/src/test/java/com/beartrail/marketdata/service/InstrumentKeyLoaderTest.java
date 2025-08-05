2960e7c570d9fded2022e02b9bfbdaae5f0be63e
georgereuben <reubengeorge101@gmail.com>
2025-08-05 16:29:29 +0530

    Add H2 database configuration for testing and update test classes to use 'test' profile

--- services/market-data/src/test/java/com/beartrail/marketdata/service/InstrumentKeyLoaderTest.java
+++ services/market-data/src/test/java/com/beartrail/marketdata/service/InstrumentKeyLoaderTest.java
@@ -3,10 +3,12 @@ package com.beartrail.marketdata.service;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class InstrumentKeyLoaderTest {
 
     @Autowired
@@ -19,4 +21,3 @@ class InstrumentKeyLoaderTest {
             .isNotEmpty();
     }
 }
-
