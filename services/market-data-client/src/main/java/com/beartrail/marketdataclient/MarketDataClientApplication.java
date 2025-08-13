c850373bd93925a38734585225ad7c72afa338be
georgereuben <reubengeorge101@gmail.com>
2025-08-13 16:41:02 +0530

    init market-data-client module with basic config

---
+++ services/market-data-client/src/main/java/com/beartrail/marketdataclient/MarketDataClientApplication.java
@@ -0,0 +1,14 @@
+package com.beartrail.marketdataclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class MarketDataClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketDataClientApplication.class, args);
+    }
+}
